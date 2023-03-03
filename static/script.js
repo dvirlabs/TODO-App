c90d1f9956f6964cc9bb805a1690ecf14a7b2e94
AmitYasharr <117912846+AmitYasharr@users.noreply.github.com>
2023-03-03 16:28:51 +0200

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,31 +1,52 @@
+var theam = "light";
+
 function onLoad() {
-  $.ajax({
-    type: 'GET',
-    url: '/table',
-    dataType: 'json',
-    success: function(data) {
-      var table = $('#pg_table');
-      $.each(data.results, function(index, row) {
-        table.append(
-          $('<tr>').append(
-            $('<td>').text(row.id),
-            $('<td>').text(row.task),
-            $('<td>').text(row.status)
-          )
-        );
-      });
-    }
-  });
-}
-
-
-
-function toggleDarkMode() {
-  var body = document.getElementsByTagName('body')[0];
-  body.classList.toggle('dark-mode');
-}
-
-
-function updateDatabase() {
+    $.ajax({
+      type: 'GET',
+      url: '/table',
+      dataType: 'json',
+      success: function(data) {
+        var table = $('#pg_table');
+        $.each(data.results, function(index, row) {
+          table.append(
+            $('<tr>').append(
+              $('<td>').text(row.id),
+              $('<td>').text(row.task),
+              $('<td>').text(row.status)
+            )
+          );
+        });
+      }
+    });
+  }
   
-}
+
+  
+ 
+
+  
+  function toggleDarkMode() {
+    const body = document.body;
+    const theam_but = document.getElementById("theme-btn")
+    const theam_but_text = document.getElementById("theam_but_text");
+    const theam_but_image = document.getElementById("theme-btn-img");
+    console.log(theam_but_image);
+    if(theam == "light"){
+      body.classList.toggle("dark_mode");
+      theam_but_text.innerHTML = "Dark";
+      theam_but_image.src = "./images/moon.svg"
+      theam = "dark";
+    }
+    else if(theam == "dark"){
+      body.classList.toggle("dark_mode");
+      theam_but_text.innerHTML = "Light";
+      theam_but_image.src = "./images/light.svg"
+      theam = "light";
+    }
+    console.log(theam)
+  }
+  
+  
+  function updateDatabase() {
+    
+  }
